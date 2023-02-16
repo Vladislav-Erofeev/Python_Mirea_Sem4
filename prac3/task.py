484a9e3fe83a9284fccaec299175463e061e6a79
Vladislav <20031911@mail.ru>
2023-02-16 12:05:21 +0300

    Added prac 3

---
+++ prac3/task.py
@@ -0,0 +1,11 @@
+def main(m, b):
+    a = 0
+    for c in range(1, m+1):
+        a += 71 - c ** 5
+    c = 0
+    for i in range(1, m+1):
+        for k in range(1, b+1):
+            c += (14 * i ** 2) + (k ** 2 + 39 * i + 0.02) ** 3 + 86 * i ** 4
+    return a - c
+
+print(main(8, 8))
